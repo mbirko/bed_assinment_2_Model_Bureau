c0b1014a9b8a5ce6b830a6f5729a7be6631e5979
mbirko <35766676+mbirko@users.noreply.github.com>
2022-10-05 11:20:12 +0200

    recreated the project with IndividualB2C auth

--- model_bureau/Controllers/WeatherForecastController.cs
+++ model_bureau/Controllers/WeatherForecastController.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
 
 namespace model_bureau.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("[controller]")]
+[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
